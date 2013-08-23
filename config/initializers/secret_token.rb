60dab345215eee3c4d3b617dad46ea7353a356c3
BitGub <shaundev.gordon@gmail.com>
2013-08-23 15:16:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YourDay::Application.config.secret_key_base = 'f71543ef7c0cedbd2404f179617c325ddfd54b0ffb01d22570850ee1c76266b894439fed18d3cdad31106e85bd6e78a359b787ae128c663517e63b34866615b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YourDay::Application.config.session_store :cookie_store, key: '_YourDay_session'
