9980ced4210d128f63a07b127d4aafe896c46b4a
BitGub <shaundev.gordon@gmail.com>
2013-08-25 18:14:23 +0100

    customised user registration form and action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable, :confirmable,
+         :token_authenticatable, #:confirmable,
        	 :lockable, :timeoutable and :omniauthable
+       	 
+       	 validates_presence_of :firstname, :surname
+       	 validates :username, :presence => true, :uniqueness => true
+       	 
 end
