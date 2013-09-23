7d3175b189a8c4aa719c19df2be495e4a87d7832
BitGub <shaundev.gordon@gmail.com>
2013-09-23 13:24:16 +0100

    deleted my_registrations_controller and created account_registration_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ActiveRecord::Base
        	 :lockable, :timeoutable and :omniauthable
        	 
        	 validates_presence_of :firstname, :surname
-       	 validates :username, :presence => true, :uniqueness => true
-       	 
+       	 validates :username, :presence => true, :uniqueness => true      	 
 end
